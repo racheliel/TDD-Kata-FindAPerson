5dac2ade2e80f8a0e3fd650c2fd19c907a52b50e
racheliel <racheli7722@gmail.com>
2015-05-13 15:09:28 +0300

    continue TDD

---
+++ Crowdmap.py
@@ -0,0 +1,9 @@
+class Crowdmap(object):
+    def __init__(self, init_list):
+      self.list = init_list
+      
+    def get_all_posts_for(self, name):
+        return [post for post in self.list if post.find(name) != -1]
+
+    def is_location_for_name(self, name):
+        return True
---
+++ FindAPersonTests.py
@@ -0,0 +1,24 @@
+
+import unittest
+from Crowdmap import Crowdmap
+
+class FindAPersonTests(unittest.TestCase):
+    def setUp(self):
+        self.crowdmap = Crowdmap(["I met Or A. at Chabad house Bangkok",
+                                  "We found Or A. R.I.P at Langtang valley",
+                                  "Missing Cowboy"])
+
+    def test_getAllPostsForName(self):
+        posts = self.crowdmap.get_all_posts_for("Or")
+        self.assertEquals(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"], posts)
+        
+    def test_getAllPostForMissingName(self):
+        posts = self.crowdmap.get_all_posts_for("Joe")
+        self.assertEquals([], posts)
+
+    def test_existingLocationInformationReturnsTrue(self):
+        location_exist = self.crowdmap.is_location_for_name("Or")
+        self.assertTrue(location_exist)
+        
+if __name__ == '__main__':
+    unittest.main()
