da9f2176f15a295226977e493006eaa0661917db
racheliel <racheli7722@gmail.com>
2015-05-13 21:29:14 +0300

    Red

--- FindAPersonTests.py
+++ FindAPersonTests.py
@@ -13,7 +13,7 @@ class FindAPersonTests(unittest.TestCase):
         self.assertEquals(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"], posts)
         
     def test_getAllPostForMissingName(self):
-        posts = self.crowdmap.get_all_posts_for("Joe")
+        posts = self.crowdmap.get_all_posts_for("Racheli")
         self.assertEquals([], posts)
 
     def test_existingLocationInformationReturnsTrue(self):
@@ -29,7 +29,7 @@ class FindAPersonTests(unittest.TestCase):
         self.assertFalse(location_exist)
 		
     def test_if_there_are_map_inconsistencies(self):
-        location_exist = self.crowdmap.if_there_are_map_inconsistencies("Racheli")
+        location_exist = self.crowdmap.if_there_are_map_inconsistencies("Or")
         self.assertFalse(location_exist)		
 		
 			
