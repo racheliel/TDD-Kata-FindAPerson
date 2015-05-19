00d153815c03361d03e4013c6a5be402655d6dde
racheliel <racheli7722@gmail.com>
2015-05-19 11:21:32 +0300

    Revert "Green - add git ignor, and test_if_there_are_map_consistencies"
    This reverts commit 980809029b7fbe9d0da687cb453196077eefeb54.

--- Crowdmap.py
+++ Crowdmap.py
@@ -4,22 +4,11 @@ class Crowdmap(object):
       
     def get_all_posts_for(self, name):
         return [post for post in self.list if post.find(name) != -1]
-		
+
     def is_location_for_name(self, name):
 		posts = [post for post in self.list if post.find(name) != -1 and post.find("at") != -1]
 		return len(posts) != 0
 		
     def if_there_are_map_inconsistencies(self, name):
-		posts = [post for post in self.list if post.find(name) != -1 and post.find("at") != -1]
-		x=posts[0].index("at")
-		print 'AA'
-		posts1 = [post for post in self.list if post.find(name) != -1 and  post.find(posts[0][x:x+4]) != -1]
-		print '\n'+posts[0][x:x+4]
-		return len(posts) != len(posts1)
-		
-    def if_there_are_map_inconsistencies(self, name):
-		posts = [post for post in self.list if post.find(name) != -1 and post.find("at") != -1]
-		x=posts[0].index("at")
-		posts1 = [post for post in self.list if post.find(name) != -1 and  post.find(posts[0][x:x+4]) != -1]
-		print '\n'+posts[0][x:x+4]
-		return len(posts) != len(posts1)
+		posts = [''.join(post.at[post.find("at")+1],post.at[post.find("at")+1]) for post in self.list if post.find(name) != -1 and post.find("at") != -1]
+		return len(posts) != 0
