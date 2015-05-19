980809029b7fbe9d0da687cb453196077eefeb54
racheliel <racheli7722@gmail.com>
2015-05-19 11:20:37 +0300

    Green - add git ignor, and test_if_there_are_map_consistencies

--- FindAPersonTests.py
+++ FindAPersonTests.py
@@ -1,4 +1,3 @@
-
 import unittest
 from Crowdmap import Crowdmap
 
@@ -28,19 +27,25 @@ class FindAPersonTests(unittest.TestCase):
         location_exist = self.crowdmap.is_location_for_name("Racheli")
         self.assertFalse(location_exist)
 		
-	def test_if_there_are_map_inconsistencies(self):
-		location_exist = self.crowdmap.if_there_are_map_inconsistencies("Or")
-		self.assertTrue(location_exist)	
+    def test_if_there_are_map_inconsistencies(self):
+        location_exist = self.crowdmap.if_there_are_map_inconsistencies("Or A.")
+        self.assertTrue(location_exist)	
 
-	def test_if_there_are_map_inconsistencies2(self):
-		crowdmap2 = Crowdmap(["I met Or A. at AAA house Bangkok",
+    def test_if_there_are_map_consistencies(self):
+        crowdmap2 = Crowdmap(["I met Or A. at AAA house Bangkok",
                                   "We found Or A. R.I.P at AAA valley",
                                   "Missing Cowboy"])
-		location_exist = crowdmap2.if_there_are_map_inconsistencies("Or")
-		self.assertFalse(location_exist)			
+        location_exist = crowdmap2.if_there_are_map_inconsistencies("Or A.")
+        self.assertFalse(location_exist)			
 
-		
-			
+    def test_if_there_are_map_inconsistencies_All_name_place(self):
+        crowdmap2 = Crowdmap(["I met Or A. at AAB house Bangkok",
+                                  "We found Or A. R.I.P at AAA valley",
+                                  "Missing Cowboy"])
+        location_exist = crowdmap2.if_there_are_map_inconsistencies("Or A.")
+        self.assertTrue(location_exist)			
+		    
+	
 		
 if __name__ == '__main__':
     unittest.main()
